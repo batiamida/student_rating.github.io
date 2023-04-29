da6b3e461a70f7b0163384b39132e37d85fbcb7e
batiamida <badfinger1991@gmail.com>
2023-04-29 20:44:06 +0300

    lab3 edit page

--- student_score/src/EditPage.js
+++ student_score/src/EditPage.js
@@ -1,64 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './static/css/edit_delete.css'
-
+import axios from "axios";
 
 function EditPage(props) {
-    return (
-        <body>
-        <div className="container">
-            <form method="POST" action="{{ url_for('update_student') }}">
-                <div className="form-group">
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" value="{{ user.username }}" />
-                </div>
 
-                <div className="form-group">
-                    <label htmlFor="firstName">First Name:</label>
-                    <input type="text" id="firstName" name="firstName" value="{{ user.firstName }}" />
-                </div>
+  const [usersData, setUsersData] = useState(null);
 
-                <div className="form-group">
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" id="lastName" name="lastName" value="{{ user.lastName }}" />
-                </div>
+  // const [username, setUsername] = useState(props.user.username);
+  // // console.log(username);
+  // const [firstName, setFirstName] = useState(props.user.firstName);
+  // const [lastName, setLastName] = useState(props.user.lastName);
+  // const [phone, setPhone] = useState(props.user.phone);
+  // const [email, setEmail] = useState(props.user.email);
 
-                <div className="form-group">
-                    <label htmlFor="phone">Phone:</label>
-                    <input type="text" id="phone" name="phone" value="{{ user.phone }}" />
-                </div>
+    async function getAuthUser() {
+    const response = await axios({
+      method: "GET",
+      url:"http://127.0.0.1:5000/get_auth_user",
+      headers: {
+        Authorization: `Bearer ${props.token}`
+      }
+    });
+    setUsersData(response.data);
+  }
+  // getAuthUser();
+  // console.log(usersData);
 
-                <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" name="email" value="{{ user.email }}" />
-                </div>
+  // const handleUsernameChange = (event) => {
+  //   setUsername(event.target.value);
+  // };
+  //
+  // const handleFirstNameChange = (event) => {
+  //   setFirstName(event.target.value);
+  // };
+  //
+  // const handleLastNameChange = (event) => {
+  //   setLastName(event.target.value);
+  // };
+  //
+  // const handlePhoneChange = (event) => {
+  //   setPhone(event.target.value);
+  // };
+  //
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // };
+  //
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   // make API call to update user data using new values
+  //   // redirect user to user listing page
+  // };
 
-                {/*<div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" />
-                </div>
+  return (
+    <body>
+    {/*  <div className="container">*/}
+    {/*    <form onSubmit={handleSubmit}>*/}
+    {/*      <div className="form-group">*/}
+    {/*        <label htmlFor="username">Username:</label>*/}
+    {/*        <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} />*/}
+    {/*      </div>*/}
 
-                <div className="form-group">
-                    <label htmlFor="confirm-password">Confirm Password:</label>
-                    <input type="password" id="confirm-password" name="confirm-password" />
-                </div>*/}
+    {/*      <div className="form-group">*/}
+    {/*        <label htmlFor="firstName">First Name:</label>*/}
+    {/*        <input type="text" id="firstName" name="firstName" value={firstName} onChange={handleFirstNameChange} />*/}
+    {/*      </div>*/}
 
-                <div className="form-buttons">
-                    <button type="submit" className="btn btn-primary">Save Changes</button>
-                    <button type="button" className="btn btn-secondary">Cancel</button>
-                </div>
-            </form>
+    {/*      <div className="form-group">*/}
+    {/*        <label htmlFor="lastName">Last Name:</label>*/}
+    {/*        <input type="text" id="lastName" name="lastName" value={lastName} onChange={handleLastNameChange} />*/}
+    {/*      </div>*/}
 
-            <hr />
+    {/*      <div className="form-group">*/}
+    {/*        <label htmlFor="phone">Phone:</label>*/}
+    {/*        <input type="text" id="phone" name="phone" value={phone} onChange={handlePhoneChange} />*/}
+    {/*      </div>*/}
 
-            {/*<h1>Delete User</h1>*/}
-            {/*<p>Are you sure you want to delete the user ""?</p>*/}
-            {/*<div className="form-buttons">*/}
-            {/*    <button type="submit" className="btn btn-danger">Delete User</button>*/}
-            {/*    <button type="button" className="btn btn-secondary">Cancel</button>*/}
-            {/*</div>*/}
-        </div>
-        </body>
-    );
+    {/*      <div className="form-group">*/}
+    {/*        <label htmlFor="email">Email:</label>*/}
+    {/*        <input type="text" id="email" name="email" value={email} onChange={handleEmailChange} />*/}
+    {/*      </div>*/}
+
+    {/*      <div className="form-buttons">*/}
+    {/*        <button type="submit" className="btn btn-primary">Save Changes</button>*/}
+    {/*        <button type="button" className="btn btn-secondary">Cancel</button>*/}
+    {/*      </div>*/}
+    {/*    </form>*/}
+
+    {/*    <hr />*/}
+
+    {/*    /!*<h1>Delete User</h1>*!/*/}
+    {/*    /!*<p>Are you sure you want to delete the user ""?</p>*!/*/}
+    {/*    /!*<div className="form-buttons">*!/*/}
+    {/*    /!*    <button type="submit" className="btn btn-danger">Delete User</button>*!/*/}
+    {/*    /!*    <button type="button" className="btn btn-secondary">Cancel</button>*!/*/}
+    {/*    /!*</div>*!/*/}
+    {/*  </div>*/}
+    </body>
+  );
 }
 
 export default EditPage;
